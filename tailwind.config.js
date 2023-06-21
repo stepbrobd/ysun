809aefd3153ec0f06e396c12f842ae2151a5d068
StepBroBD <Hi@StepBroBD.com>
2023-06-21 15:27:01 -0600

    remove intro template

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     content: [
         "./app/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
-        "./intro-template/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         ...theme,
