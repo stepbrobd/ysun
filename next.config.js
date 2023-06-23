158bd9e6f7d848925c637166f2bd4dbe42d6b44a
StepBroBD <Hi@StepBroBD.com>
2023-06-23 15:22:58 -0600

    nextra extra options

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   latex: true,
+  staticImage: true,
+  defaultShowCopyCode: true,
+  readingTime: true,
 });
 
 module.exports = withNextra();
