29fe670c01e282d848ac4cc26d4654f60ec386c4
Yifei Sun <ysun@hey.com>
2023-12-28 22:09:17 -0500

    disable fresh partials

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -24,7 +24,7 @@ const app = ({ Component }: PageProps): JSX.Element => {
         data-color-mode="auto"
         data-light-theme="light"
         data-dark-theme="dark"
-        f-client-nav
+        f-client-nav={false}
       >
         <Partial name="body">
           <Component />
