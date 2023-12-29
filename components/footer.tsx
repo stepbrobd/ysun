66b154f4d5daf9721d9a64021ef25516116cd14b
Yifei Sun <ysun@hey.com>
2023-12-29 17:24:16 -0500

    add version and bin path

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,4 @@
+import { cwd } from "!libs/page/cwd.ts";
 import IconCreativeCommons from "$icons/creative-commons.tsx";
 import IconCreativeCommonsBy from "$icons/creative-commons-by.tsx";
 import IconCreativeCommonsNc from "$icons/creative-commons-nc.tsx";
@@ -6,8 +7,25 @@ import { JSX } from "preact/jsx-runtime";
 const Footer = (): JSX.Element => {
   return (
     <footer class="flex flex-col gap-4 py-4">
-      <div class="flex flex-row justify-between">
-        <small class="tracking-tighter font-light text-xs text-neutral-600 dark:text-neutral-400">
+      <div class="markdown-body">
+        <pre>
+          <span>DENO_DEPLOYMENT_ID</span>
+          <span class="text-indigo-600 dark:text-indigo-300">=</span>
+          <span class="text-indigo-600 dark:text-indigo-300">
+            <a class="no-underline" href={`https://github.com/stepbrobd/ysun/commit/${Deno.env.get("DENO_DEPLOYMENT_ID")}`} target="_blank" rel="noopener noreferrer">
+              {Deno.env.get("DENO_DEPLOYMENT_ID")}
+            </a>
+          </span>
+          <span>{" "}</span>
+          <span class="text-green-700 dark:text-green-400">
+            <a class="no-underline" href={`https://github.com/stepbrobd/ysun/blob/${Deno.env.get("DENO_DEPLOYMENT_ID")}/flake.nix`} target="_blank" rel="noopener noreferrer">
+              {cwd}/ysun
+            </a>
+          </span>
+        </pre>
+      </div>
+      <div class="flex flex-row justify-between text-neutral-600 dark:text-neutral-400">
+        <small class="tracking-tighter font-light text-xs">
           &copy; {new Date().getFullYear()}
         </small>
         <a
