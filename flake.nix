7a1bdad8e6138b51f04dc9431bbc84bdd898c2e9
Yifei Sun <ysun@hey.com>
2024-01-15 11:54:26 -0500

    nix fmt now includes deno fmt

--- flake.nix
+++ flake.nix
@@ -50,7 +50,10 @@
       };
     in
     {
-      formatter = pkgs.nixpkgs-fmt;
+      formatter = pkgs.writeShellScriptBin "formatter" ''
+        ${pkgs.deno}/bin/deno fmt .
+        ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
+      '';
 
       packages.default = drv;
 
