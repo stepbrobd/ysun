85e7a2c582e5e6b206bcd690e22b0c2d5506638d
Yifei Sun <ysun@hey.com>
2024-07-03 01:05:47 +0900

    use local deployment

--- flake.nix
+++ flake.nix
@@ -12,30 +12,49 @@
       (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
       in
       {
-        packages.default = pkgs.stdenvNoCC.mkDerivation {
-          name = "ysun";
-          src = ./.;
-          configurePhase = ''
-            runHook preConfigure
-            unset HOME
-            unset XDG_CACHE_HOME
-            export HOME=$TMPDIR/home
-            export DENO_DIR=$TMPDIR/deno
-            runHook postConfigure
-          '';
-          nativeBuildInputs = with pkgs; [ deno ];
-          buildPhase = ''
-            runHook preBuild
-            deno task lume
-            runHook postBuild
-          '';
-          installPhase = ''
-            runHook preInstall
-            mkdir -p $out/var/www/html
-            cp -r outputs/* $out/var/www/html
-            runHook postInstall
+        packages = rec {
+          site = pkgs.stdenvNoCC.mkDerivation {
+            name = "ysun";
+            src = ./.;
+            configurePhase = ''
+              runHook preConfigure
+              unset HOME
+              unset XDG_CACHE_HOME
+              export HOME=$TMPDIR/home
+              export DENO_DIR=$TMPDIR/deno
+              runHook postConfigure
+            '';
+            nativeBuildInputs = with pkgs; [ deno ];
+            buildPhase = ''
+              runHook preBuild
+              deno task lume
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/var/www/html
+              cp -r outputs/* $out/var/www/html
+              runHook postInstall
+            '';
+          };
+
+          image = pkgs.dockerTools.buildLayeredImage {
+            name = "ysun";
+            tag = "latest";
+            config = {
+              Cmd = [ "${pkgs.caddy}/bin/caddy" "run" "--config" "${./caddyfile}" ];
+              Env = [ "SITE_ROOT=${site}/var/www/html" "SITE_PORT=80" ];
+            };
+          };
+
+          deploy = pkgs.writeShellScriptBin "deploy" ''
+            set -euxo pipefail
+            export PATH="${lib.makeBinPath [(pkgs.docker.override { clientOnly = true; }) pkgs.flyctl]}:$PATH"
+            image=$(docker load < ${image} | awk '{ print $3; }')
+            flyctl deploy --image $image --local-only
           '';
         };
 
