545b481265820217fb8c7f0e589b7709172983f0
Yifei Sun <ysun@hey.com>
2024-07-03 14:26:38 +0900

    lume cms init

--- _cms.ts
+++ _cms.ts
@@ -1,14 +1,34 @@
-import lumeCMS, { GitHub } from "lume/cms/mod.ts";
+import lumeCMS from "lume/cms/mod.ts";
+import GitHub from "lume/cms/storage/github.ts";
 import { Octokit } from "octokit";
+
+const user = Deno.env.get("USER");
+const pass = Deno.env.get("PASS");
 
 const cms = lumeCMS({
   site: { name: "CMS" },
   auth: {
     method: "basic",
-    users: {
-      "ysun@hey.com": "password1",
-    },
+    users: { [user]: pass },
   },
 });
+
+cms.storage(
+  "gh",
+  new GitHub({
+    client: new Octokit({ auth: Deno.env.get("TOKEN") }),
+    owner: "stepbrobd",
+    repo: "ysun",
+  }),
+);
+
+cms.collection("pages", "gh:pages/*.md", [
+  { name: "title", type: "text", attributes: { required: true } },
+  { name: "date", type: "datetime", value: new Date(), attributes: { required: true } },
+  { name: "content", type: "markdown", attributes: { required: true } },
+]);
+
+cms.upload("images", "gh:assets/static/img");
+cms.upload("documents", "gh:assets/static/doc");
 
 export default cms;
