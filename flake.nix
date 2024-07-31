db443b4c522191ce53a20cd405091f02b8bd61b7
Yifei Sun <ysun@hey.com>
2024-07-31 14:22:10 +0900

    use autopilot

--- flake.nix
+++ flake.nix
@@ -4,19 +4,23 @@
     parts.url = "github:hercules-ci/flake-parts";
     parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     systems.url = "github:nix-systems/default";
+    autopilot.url = "github:stepbrobd/autopilot";
+    autopilot.inputs.nixpkgs.follows = "nixpkgs";
+    autopilot.inputs.parts.follows = "parts";
+    autopilot.inputs.systems.follows = "systems";
   };
 
-  outputs = inputs: inputs.parts.lib.mkFlake
+  outputs = inputs: inputs.autopilot.lib.mkFlake
     {
       inherit inputs;
+
       specialArgs.configs = {
         name = "ysun";
         port = 8080;
         cms = 3000;
       };
+
+      autopilot.parts.path = ./parts;
     }
-    {
-      systems = import inputs.systems;
-      imports = [ ./parts ];
-    };
+    { systems = import inputs.systems; };
 }
