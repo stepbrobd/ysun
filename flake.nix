a59411a0d4f979f40487f4e27fc430c3e9ea26ff
Yifei Sun <ysun@hey.com>
2025-01-28 15:04:46 -0500

    cleanup

--- flake.nix
+++ flake.nix
@@ -1,26 +1,30 @@
 {
+  outputs = { nixpkgs, parts, systems, ... } @ inputs: parts.lib.mkFlake { inherit inputs; } {
+    systems = import systems;
+
+    perSystem = { pkgs, ... }: {
+      _module.args.lib = builtins // nixpkgs.lib // parts.lib;
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          deno
+          direnv
+          git
+          nix-direnv
+        ];
+      };
+
+      formatter = pkgs.writeShellScriptBin "formatter" ''
+        ${pkgs.deno}/bin/deno fmt .
+        ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
+      '';
+    };
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     parts.url = "github:hercules-ci/flake-parts";
     parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     systems.url = "github:nix-systems/default";
-    autopilot.url = "github:stepbrobd/autopilot";
-    autopilot.inputs.nixpkgs.follows = "nixpkgs";
-    autopilot.inputs.parts.follows = "parts";
-    autopilot.inputs.systems.follows = "systems";
   };
-
-  outputs = inputs: inputs.autopilot.lib.mkFlake
-    {
-      inherit inputs;
-
-      specialArgs.configs = {
-        name = "ysun";
-        port = 8080;
-        cms = 3000;
-      };
-
-      autopilot.parts.path = ./parts;
-    }
-    { systems = import inputs.systems; };
 }
--- parts/formatter.nix
+++
@@ -1,8 +0,0 @@
-{
-  perSystem = { pkgs, ... }: {
-    formatter = pkgs.writeShellScriptBin "formatter" ''
-      ${pkgs.deno}/bin/deno fmt .
-      ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
-    '';
-  };
-}
