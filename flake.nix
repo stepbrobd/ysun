5da0f96ce2127769264c1c4fe5af0d6d7e7a4662
Yifei Sun <ysun@hey.com>
2025-05-08 21:50:27 -0400

    drop release workflow

--- flake.nix
+++ flake.nix
@@ -1,14 +1,11 @@
 {
-  outputs = { self, nixpkgs, parts, systems, deno } @ inputs: parts.lib.mkFlake { inherit inputs; } {
+  outputs = { self, nixpkgs, parts, systems } @ inputs: parts.lib.mkFlake { inherit inputs; } {
     systems = import systems;
 
     perSystem = { pkgs, system, ... }: {
       _module.args = {
         lib = builtins // nixpkgs.lib // parts.lib;
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ deno.overlays.default ];
-        };
+        pkgs = import nixpkgs { inherit system; };
       };
 
       devShells.default = pkgs.mkShell {
@@ -24,65 +21,6 @@
         ${pkgs.deno}/bin/deno fmt .
         ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt .
       '';
-
-      packages.site = pkgs.denoPlatform.mkDenoDerivation rec {
-        name = "site";
-        pname = name;
-        stdenv = pkgs.stdenvNoCC;
-        src = ./.;
-        buildPhase = "deno task build";
-        installPhase = ''
-          mkdir -p $out
-          cp -r outputs/* $out
-        '';
-      };
-
-      packages.exec = pkgs.denoPlatform.mkDenoBinary rec {
-        name = "exec";
-        pname = name;
-        src = ./.;
-        unstable = true;
-        permissions.allow = {
-          env = true;
-          sys = true;
-          net = true;
-          read = true;
-        };
-        entryPoint = "_main.ts";
-        buildPhase = ''
-          deno compile ${pkgs.denoPlatform.lib.generateFlags {
-            inherit permissions unstable entryPoint;
-            additionalDenoArgs = ["--output" name];
-          }}
-        '';
-      };
-
-      # workaround
-      # build without sandbox on gha
-      # and import as pure derivation
-      packages.default =
-        let
-          importnar = name: url: hash: import <nix/fetchurl.nix> {
-            inherit name url hash;
-            unpack = true;
-          };
-          exec = importnar
-            "exec"
-            "https://github.com/stepbrobd/ysun/releases/download/2025.04.03.19.00.10/exec.nar"
-            "sha256-0dAXdWdJPaf7Cqc3FSKKCRFdj/Wpuk4VCwjtvv2Oysk=";
-          site = importnar
-            "site"
-            "https://github.com/stepbrobd/ysun/releases/download/2025.04.03.19.00.10/site.nar"
-            "sha256-2wVeHbahgbXBGsAZayUlxN+F7KyE4vKQXvrI9y3wc8o=";
-        in
-        pkgs.writeShellApplication {
-          meta.platforms = [ "x86_64-linux" ];
-          name = "ysun";
-          runtimeInputs = [ exec site ];
-          text = ''
-            exec ${exec}/bin/exec ${site} "$@"
-          '';
-        };
     };
   };
 
@@ -91,7 +29,5 @@
     parts.url = "github:hercules-ci/flake-parts";
     parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     systems.url = "github:nix-systems/default";
-    deno.url = "github:nekowinston/nix-deno";
-    deno.inputs.nixpkgs.follows = "nixpkgs";
   };
 }
