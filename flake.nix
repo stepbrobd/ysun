81b71bb22afcb41fed0a7dab9fbecd23b342d3f8
Yifei Sun <ysun@hey.com>
2025-11-14 20:33:41 +0100

    add vw link

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
         src = ./.;
         outputHashAlgo = "sha256";
         outputHashMode = "recursive";
-        outputHash = "sha256-XAHvHr8YLeJxmcEjVs3uYZrp2CM60af+32BNr1l5AwU=";
+        outputHash = "sha256-t1PSO6w96CFY0I4HLIDJVCqzQrdpeGbbwNKgTVsYDU0=";
         nativeBuildInputs = [ pkgs.deno ];
         buildPhase = ''
           runHook preBuild
