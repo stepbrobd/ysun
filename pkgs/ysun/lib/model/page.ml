ed50cc03b24028dc04e8ac182fca748f2a877f0f
Yifei Sun <ysun@hey.com>
2026-02-11 11:09:18 +0100

    simplify model

--- pkgs/ysun/lib/model/page.ml
+++ pkgs/ysun/lib/model/page.ml
@@ -1,33 +1,106 @@
-open Model_util
-
 type t =
-  { page_title : string option
+  { title : string option
   ; description : string option
-  ; sub_path : string option
+  ; date : string option
+  ; words : int option
+  ; minutes : int option
+  ; list : bool
+  ; layout : string option
+  ; external_ : string option
+  ; hidden : bool
   }
 
-let make ?title ?description ?sub_path () = { page_title = title; description; sub_path }
+let make
+      ?title
+      ?description
+      ?date
+      ?words
+      ?minutes
+      ?(list = false)
+      ?layout
+      ?external_
+      ?(hidden = false)
+      ()
+  =
+  { title; description; date; words; minutes; list; layout; external_; hidden }
+;;
+
 let entity_name = "Page"
 let empty = make ()
 let neutral = Ok empty
 
 let validate_underlying_page fields =
   let open Yocaml.Data.Validation in
-  let+ page_title = optional fields "page_title" string
+  let+ title = optional fields "title" string
   and+ description = optional fields "description" string
-  and+ sub_path = optional fields "sub_path" string in
-  { page_title; description; sub_path }
+  and+ date = optional fields "date" string
+  and+ layout = optional fields "layout" string
+  and+ external_ = optional fields "external" string
+  and+ hidden = optional fields "hidden" bool in
+  let hidden = Option.value ~default:false hidden in
+  { title
+  ; description
+  ; date
+  ; words = None
+  ; minutes = None
+  ; list = false
+  ; layout
+  ; external_
+  ; hidden
+  }
 ;;
 
 let validate = Yocaml.Data.Validation.record validate_underlying_page
 
-let normalize { page_title; description; sub_path } =
+let normalize
+      { title
+      ; description
+      ; date
+      ; words
+      ; minutes
+      ; list = is_list
+      ; layout
+      ; external_
+      ; hidden
+      }
+  =
   let open Yocaml.Data in
-  [ "has_page_title", has_opt page_title
-  ; "page_title", option string page_title
-  ; "has_description", has_opt description
+  [ "title", option string title
   ; "description", option string description
-  ; "has_sub_path", has_opt sub_path
-  ; "sub_path", option string sub_path
+  ; "date", option string date
+  ; "words", option int words
+  ; "minutes", option int minutes
+  ; "list", bool is_list
+  ; "layout", option string layout
+  ; "external", option string external_
+  ; "hidden", bool hidden
   ]
+;;
+
+let get_url file =
+  let open Yocaml in
+  let filename = Path.basename file |> Option.value ~default:"" in
+  let name = Filename.remove_extension filename in
+  if name = "home" then "/" else "/" ^ name ^ "/"
+;;
+
+let count_words str =
+  let regexp = Str.regexp "[ \n\r\t]+" in
+  let words = Str.split regexp str in
+  List.length words
+;;
+
+type template_type =
+  | Page
+  | Other of string
+
+let resolve_template meta =
+  let default = "page.liquid" in
+  match meta.layout with
+  | None -> Page, default
+  | Some l ->
+    let base = Filename.remove_extension l in
+    let ext = Filename.extension l in
+    let resolved = if ext = ".vto" then base ^ ".liquid" else l in
+    if resolved = "page.liquid" then Page, resolved else Other resolved, resolved
 ;;
--- pkgs/ysun/lib/model/page.mli
+++ pkgs/ysun/lib/model/page.mli
@@ -1,16 +1,42 @@
-(** Describes a generic page, mostly used on top of another model. *)
-
-(** The type describing a page. *)
-type t
+type t =
+  { title : string option
+  ; description : string option
+  ; date : string option
+  ; words : int option
+  ; minutes : int option
+  ; list : bool
+  ; layout : string option
+  ; external_ : string option
+  ; hidden : bool
+  }
 
 val empty : t
-val make : ?title:string -> ?description:string -> ?sub_path:string -> unit -> t
+
+val make
+  :  ?title:string
+  -> ?description:string
+  -> ?date:string
+  -> ?words:int
+  -> ?minutes:int
+  -> ?list:bool
+  -> ?layout:string
+  -> ?external_:string
+  -> ?hidden:bool
+  -> unit
+  -> t
 
 val validate_underlying_page
   :  (string * Yocaml.Data.t) list
   -> t Yocaml.Data.Validation.validated_record
 
-(** {1 Dealing as metadata} *)
-
 include Yocaml.Required.DATA_READABLE with type t := t
 include Yocaml.Required.DATA_INJECTABLE with type t := t
+
+val get_url : Yocaml.Path.t -> string
+val count_words : string -> int
+
+type template_type =
+  | Page
+  | Other of string
+
+val resolve_template : t -> template_type * string
