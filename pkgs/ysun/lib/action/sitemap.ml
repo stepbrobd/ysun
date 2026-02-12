18fb400c31bdff26c472ce2bacd2d4ba65e69e23
Yifei Sun <ysun@hey.com>
2026-02-12 22:06:52 +0100

    use Yocaml_syndication.Xml for sitemap

--- pkgs/ysun/lib/action/sitemap.ml
+++ pkgs/ysun/lib/action/sitemap.ml
@@ -1,40 +1,27 @@
-let escape_xml s =
-  let buf = Buffer.create (String.length s) in
-  String.iter
-    (function
-      | '&' -> Buffer.add_string buf "&amp;"
-      | '<' -> Buffer.add_string buf "&lt;"
-      | '>' -> Buffer.add_string buf "&gt;"
-      | '"' -> Buffer.add_string buf "&quot;"
-      | '\'' -> Buffer.add_string buf "&apos;"
-      | c -> Buffer.add_char buf c)
-    s;
-  Buffer.contents buf
+let make_url_node (meta, url, _file) =
+  let open Model.Page in
+  let open Yocaml_syndication.Xml in
+  let loc = leaf ~name:"loc" (escape (Config.site_url ^ url)) in
+  let lastmod = may_leaf ~name:"lastmod" ~finalize:escape Fun.id meta.date in
+  node ~name:"url" [ loc; lastmod ]
 ;;
 
 let generate sorted_pages =
-  let buf = Buffer.create 4096 in
-  Buffer.add_string buf "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-  Buffer.add_string buf "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
-  List.iter
-    (fun (meta, url, _file) ->
-       let open Model.Page in
-       if (not meta.hidden) && meta.redirect = None
-       then (
-         Buffer.add_string buf "  <url>\n";
-         Buffer.add_string buf "    <loc>";
-         Buffer.add_string buf (escape_xml (Config.site_url ^ url));
-         Buffer.add_string buf "</loc>\n";
-         (match meta.date with
-          | Some d ->
-            Buffer.add_string buf "    <lastmod>";
-            Buffer.add_string buf (escape_xml d);
-            Buffer.add_string buf "</lastmod>\n"
-          | None -> ());
-         Buffer.add_string buf "  </url>\n"))
-    sorted_pages;
-  Buffer.add_string buf "</urlset>\n";
-  Buffer.contents buf
+  let open Yocaml_syndication.Xml in
+  let entries =
+    sorted_pages
+    |> List.filter (fun (meta, _, _) ->
+      let open Model.Page in
+      (not meta.hidden) && meta.redirect = None)
+    |> List.map make_url_node
+  in
+  let urlset =
+    node
+      ~name:"urlset"
+      ~attr:[ Attr.string ~key:"xmlns" "http://www.sitemaps.org/schemas/sitemap/0.9" ]
+      entries
+  in
+  document urlset |> to_string
 ;;
 
 let run (module R : Sigs.RESOLVER) (cache, sorted_pages) =
