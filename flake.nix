a8a9d37a592853590583aa00639256ea933c79cc
Yifei Sun <ysun@hey.com>
2026-03-12 21:10:24 +0100

    formatter in any dir

--- flake.nix
+++ flake.nix
@@ -31,9 +31,20 @@
       };
 
       formatter = pkgs.writeShellScriptBin "formatter" ''
+        set -eoux pipefail
+        shopt -s globstar
+        root="$PWD"
+        while [[ ! -f "$root/.git/index" ]]; do
+          if [[ "$root" == "/" ]]; then
+            exit 1
+          fi
+          root="$(dirname "$root")"
+        done
+        pushd "$root" > /dev/null
         ${lib.getExe pkgs.deno} fmt pages
         ${lib.getExe pkgs.nixpkgs-fmt} .
         ${lib.getExe pkgs.ocamlPackages.dune} fmt
+        popd
       '';
 
       packages = with lib; fix (self: (
