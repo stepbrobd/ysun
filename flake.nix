12b99eb9fd2d7ebe45721e1de11f08ba1b7a654c
Yifei Sun <ysun@hey.com>
2026-08-19 19:55:11 +0200

    use a simpler git root finder

--- flake.nix
+++ flake.nix
@@ -34,14 +34,7 @@
       formatter = pkgs.writeShellScriptBin "formatter" ''
         set -eoux pipefail
         shopt -s globstar
-        root="$PWD"
-        while [[ ! -f "$root/.git/index" ]]; do
-          if [[ "$root" == "/" ]]; then
-            exit 1
-          fi
-          root="$(dirname "$root")"
-        done
-        pushd "$root" > /dev/null
+        pushd "$(${lib.getExe pkgs.git} rev-parse --show-toplevel)" > /dev/null
         ${lib.getExe pkgs.deno} fmt pages
         ${lib.getExe pkgs.nixpkgs-fmt} .
         ${lib.getExe pkgs.ocamlPackages.dune} fmt
