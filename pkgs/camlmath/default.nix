4e2da9be7166814923d19eeed5e4d2f65110dd51
Yifei Sun <ysun@hey.com>
2026-09-03 22:51:26 +0200

    pkgs/camlmath: 2026.903.0 -> 2026.903.1

--- pkgs/camlmath/default.nix
+++ pkgs/camlmath/default.nix
@@ -5,13 +5,13 @@
 
 buildDunePackage (finalAttrs: {
   pname = "camlmath";
-  version = "2026.903.0";
+  version = "2026.903.1";
 
   src = fetchFromGitHub {
     owner = "stepbrobd";
     repo = "camlmath";
     tag = finalAttrs.version;
-    hash = "sha256-yNCEpoXDOn+lJMquvVvZTeMcKETBjw3yz+ht9toFHqk=";
+    hash = "sha256-+E4xXsN70o5j7JQLvM6onyuAzE9JQ1v/aDGXVNSvAf4=";
   };
 
   env.DUNE_CACHE = "disabled";
